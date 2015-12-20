0a1f2e4b5b68ca36a36df094e58b6c5f7d394b25
metarmask <metarmask@users.noreply.github.com>
2015-12-20 18:41:33 +0100

    Click on icon to stop

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
 chrome.runtime.onInstalled.addListener(function() {
+	console.debug("runtime.onInstalled");
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([{
 			conditions: [
@@ -12,33 +13,68 @@ chrome.runtime.onInstalled.addListener(function() {
 			actions: [new chrome.declarativeContent.ShowPageAction()]
 		}]);
 	});
+	chrome.storage.local.set({playingTabs:[]}); // Reloading the extension doesn't trigger onStartup
+});
+
+chrome.runtime.onStartup.addListener(function() {
+	console.debug("runtime.onStartup");
+	chrome.storage.local.set({playingTabs:[]});
 });
 
 function startPlaying(tabID) {
 	console.log("Playing in tab with ID " + tabID);
 	chrome.pageAction.setIcon({tabId:tabID,path:"images/pageAction/enabled.png"});
+	chrome.pageAction.setTitle({tabId:tabID,title:"When this video finishes the tab to the right will be opened"});
 	chrome.tabs.update(tabID,{active:true});
 	chrome.tabs.executeScript(tabID,{file:"content-script.js"});
+	chrome.storage.local.get("playingTabs",function(items){
+		items.playingTabs.push(tabID);
+		chrome.storage.local.set({playingTabs:items.playingTabs});
+	});
+}
+
+function stopPlaying(tabID) {
+	console.log("No longer playing in tab with ID " + tabID);
+	chrome.storage.local.get("playingTabs",function(items){
+		var index = items.playingTabs.indexOf(tabID);
+		if(index != -1) {
+			items.playingTabs.splice(index,1);
+		}
+		chrome.storage.local.set({playingTabs:items.playingTabs});
+	});
+	var manifest = chrome.runtime.getManifest();
+	chrome.pageAction.setIcon({tabId:tabID,path:manifest.page_action.default_icon});
+	chrome.pageAction.setTitle({tabId:tabID,title:manifest.page_action.default_title});
 }
 
 chrome.pageAction.onClicked.addListener(function(tab){
-	startPlaying(tab.id);
+	chrome.storage.local.get("playingTabs",function(items){
+		if(items.playingTabs.indexOf(tab.id) == -1) {
+			startPlaying(tab.id);
+		}else{
+			stopPlaying(tab.id);
+		}
+	});
 });
 
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
-	console.log("runtime.onMessage: ",arguments);
+	console.debug("runtime.onMessage: ",arguments);
 	if(message == "ended" && sender.tab !== undefined) {
-		console.log("Got \"ended\"-message, querying tabs");
-		chrome.tabs.query({windowId:sender.tab.windowId,index:sender.tab.index+1},function(tabs){
-			console.log("Queried tabs",tabs);
-			if(tabs[0] !== undefined) {
-				var url = new URL(tabs[0].url);
-				if(!(url.host == "www.youtube.com" && url.pathname == "/watch")) {
-					console.log("The next tab wasn't a youtube one.");
-					return;
-				}
-				startPlaying(tabs[0].id);
-				chrome.tabs.remove(sender.tab.id);
+		console.log("Got \"ended\"-message, checking if it is playing");
+		chrome.storage.local.get("playingTabs",function(items){
+			if(items.playingTabs.indexOf(sender.tab.id) != -1) {
+				chrome.tabs.query({windowId:sender.tab.windowId,index:sender.tab.index+1},function(tabs){
+					console.log("Queried tabs",tabs);
+					if(tabs[0] !== undefined) {
+						var url = new URL(tabs[0].url);
+						if(!(url.host == "www.youtube.com" && url.pathname == "/watch")) {
+							console.log("The next tab wasn't a youtube one.");
+							return;
+						}
+						startPlaying(tabs[0].id);
+						chrome.tabs.remove(sender.tab.id);
+					}
+				});
 			}
 		});
 	}
--- content-script.js
+++ content-script.js
@@ -1,10 +1,15 @@
-var scriptTag = document.createElement("script");
-scriptTag.src = chrome.runtime.getURL("injection-script.js");
-scriptTag.async = true;
-document.head.appendChild(scriptTag);
-console.log("Script injected");
-window.addEventListener("message",function(){
-	if(event.data == "tabplaylist: ended") {
-		chrome.runtime.sendMessage("ended");
-	}
-});
+var alreadyExistingTag = document.querySelector("script[async][src='chrome-extension://afcmfnlbcoginnfdkokfgpcmooekakhf/injection-script.js']");
+if(alreadyExistingTag) {
+	console.log("Already injected");
+}else{
+	var scriptTag = document.createElement("script");
+	scriptTag.src = chrome.runtime.getURL("injection-script.js");
+	scriptTag.async = true;
+	document.head.appendChild(scriptTag);
+	console.log("Script injected");
+	window.addEventListener("message",function(){
+		if(event.data == "tabplaylist: ended") {
+			chrome.runtime.sendMessage("ended");
+		}
+	});
+}
