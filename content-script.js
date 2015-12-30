ca0e602fbb197e2ca10d336ec74809e68e6a6c88
metarmask <metarmask@users.noreply.github.com>
2015-12-30 14:39:22 +0100

    Changed name of injection-script.js

--- content-script.js
+++ content-script.js
@@ -1,7 +1,7 @@
-var alreadyExistingTag = document.querySelector("script[async][src='chrome-extension://afcmfnlbcoginnfdkokfgpcmooekakhf/injection-script.js']");
+var alreadyExistingTag = document.querySelector("script[async][src='chrome-extension://afcmfnlbcoginnfdkokfgpcmooekakhf/content-script-js-accesser.js']");
 if(!alreadyExistingTag) {
 	var scriptTag = document.createElement("script");
-	scriptTag.src = chrome.runtime.getURL("injection-script.js");
+	scriptTag.src = chrome.runtime.getURL("content-script-js-accesser.js");
 	scriptTag.async = true;
 	document.head.appendChild(scriptTag);
 	window.addEventListener("message",function(){
