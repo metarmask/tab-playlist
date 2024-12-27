9dfe2e15167fcf04a295b582322de558af7259ad
Jeradan <jclason1@gmail.com>
2024-12-26 23:43:08 -0500

    Removed dumb comment

--- content-script-js-accesser.js
+++ content-script-js-accesser.js
@@ -1,7 +1,7 @@
 var playerDiv = document.querySelector("#movie_player");
 playerDiv.playVideo();
 playerDiv.addEventListener("onStateChange", function(state) {
-	if(state === 0) { /* Ended event from video player has state of 0 */
+	if(state === 0) { /* Ended */
 		window.postMessage("tabplaylist: ended", "*");
 	}
 });
