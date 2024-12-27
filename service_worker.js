4fd7485a7a509d7e132a561860bf47e32d590889
Jeradan <jclason1@gmail.com>
2024-12-26 23:40:48 -0500

    Updated service_worker.js to use Promises instead of callbacks. This is part of the Manifest V3 migration checklist

--- service_worker.js
+++ service_worker.js
@@ -25,14 +25,14 @@ function startPlaying(tabID) {
 	chrome.action.setTitle({ tabId: tabID, title: manifest.action.enabled_title });
 	chrome.tabs.update(tabID, { active: true });
 	chrome.scripting.executeScript({ target: { tabId: tabID }, files: ["content-script.js"] });
-	chrome.storage.local.get("playingTabs", function(items) {
+	chrome.storage.local.get("playingTabs").then((items) => {
 		items.playingTabs.push(tabID);
 		chrome.storage.local.set({ playingTabs: items.playingTabs });
 	});
 }
 
 function stopPlaying(tabID) {
-	chrome.storage.local.get("playingTabs", function(items) {
+	chrome.storage.local.get("playingTabs").then((items) => {
 		var index = items.playingTabs.indexOf(tabID);
 		if(index != -1) {
 			items.playingTabs.splice(index, 1);
@@ -45,7 +45,7 @@ function stopPlaying(tabID) {
 }
 
 chrome.action.onClicked.addListener(function(tab) {
-	chrome.storage.local.get("playingTabs", function(items) {
+	chrome.storage.local.get("playingTabs").then((items) => {
 		if(items.playingTabs.indexOf(tab.id) == -1) {
 			startPlaying(tab.id);
 		} else {
@@ -57,10 +57,10 @@ chrome.action.onClicked.addListener(function(tab) {
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 	if(message == "ended" && sender.tab !== undefined) {
 		// Received video ended message from player. Attempting to switch tabs
-		chrome.storage.local.get("playingTabs", function(items) {
+		chrome.storage.local.get("playingTabs").then((items) => {
 			if(items.playingTabs.indexOf(sender.tab.id) != -1) {
 				// Find the next tab to the right (+1) in our original window
-				chrome.tabs.query({ windowId: sender.tab.windowId, index: sender.tab.index+1 }, function(tabs) {
+				chrome.tabs.query({ windowId: sender.tab.windowId, index: sender.tab.index+1 }).then((tabs) => {
 					if(tabs[0] !== undefined) {
 						// Found a tab, make sure its Youtube before we switch to it
 						var url = new URL(tabs[0].url);
@@ -72,7 +72,7 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
 						startPlaying(tabs[0].id);
 
 						// Optionally close the tab that just finished playing
-						chrome.storage.sync.get({"option-closeTab":true}, function(items){
+						chrome.storage.sync.get({"option-closeTab":true}).then((items) => {
 							if(items["option-closeTab"] === true) {
 								chrome.tabs.remove(sender.tab.id);
 							}
